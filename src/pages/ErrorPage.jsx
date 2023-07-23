6032f79db70d96e62d737be8987e2eca4943330b
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-07-24 01:34:27 +0600

    added ErrorPage Component

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Failed to load the page you wanted!!!</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
+import ErrorPage from "../pages/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
