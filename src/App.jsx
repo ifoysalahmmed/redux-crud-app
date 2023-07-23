03c9cfe96543cb534274c78ec8a22b4043a5e195
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-07-24 02:08:45 +0600

    added Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./layouts/Navbar";
+import Footer from "./layouts/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Outlet></Outlet>
       </main>
+      <Footer></Footer>
     </>
   );
 }
