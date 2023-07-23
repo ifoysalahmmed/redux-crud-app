65bcc81ec40d36c0dacacc903adf402b3e926309
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-07-24 01:29:54 +0600

    added Home Component

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
