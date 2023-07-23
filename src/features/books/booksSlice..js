546a4a1bc9f06e541624100a0c4bc1c72cfe024b
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-07-24 02:16:52 +0600

    added booksSlice file

---
+++ src/features/books/booksSlice..js
@@ -0,0 +1,43 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const initialBooks = {
+  books: [
+    {
+      id: 1,
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+    },
+    {
+      id: 2,
+      title: "1984",
+      author: "George Orwell",
+    },
+    {
+      id: 3,
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+    },
+    {
+      id: 4,
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+    },
+    {
+      id: 5,
+      title: "The Catcher in the Rye",
+      author: "J.D. Salinger",
+    },
+  ],
+};
+
+export const booksSlice = createSlice({
+  name: "books",
+  initialState: initialBooks,
+  reducers: {
+    showBooks: (state) => state,
+  },
+});
+
+export const { showBooks } = booksSlice.actions;
+
+export default booksSlice.reducer;
