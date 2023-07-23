77e2927db67a00046ce79c3bf4d01390eaf11b25
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-07-24 01:38:27 +0600

    added Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import Navbar from "./layouts/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
       <Outlet></Outlet>
     </>
   );
