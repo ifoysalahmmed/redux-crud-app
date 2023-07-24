6cbbfb86bfd19571cb178784e640825a01898739
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-07-25 00:20:01 +0600

    updated AddBook Component

--- src/features/books/AddBook.jsx
+++ src/features/books/AddBook.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 const AddBook = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const numberOfBooks = useSelector((state) => state.booksReducer.books.length);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const book = {
+      id: numberOfBooks++,
+      title,
+      author,
+    };
+  };
+
   return (
     <div>
       <h2>Add Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label htmlFor="title">Title: </label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-field">
+          <label htmlFor="author">Author: </label>
+          <input
+            type="text"
+            name="author"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Add Book</button>
+      </form>
     </div>
   );
 };
